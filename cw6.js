4cb82b6b4268c76ceb45e11b8ca677eaee36bcf8
Hh20082023 <140391284+Hh20082023@users.noreply.github.com>
2023-07-28 12:43:43 +0300

    Add files via upload

---
+++ cw6.js
@@ -0,0 +1,22 @@
+function BMI(weight, height) {
+    return weight / (height * height);
+    console.log(BMI);
+}
+let BMI1 = BMI (60, 1.6);
+
+
+function Status () {
+    let Status = BMI(weight, height);
+
+    if (BMI < 18.5 ){console.log (" لديك نقص في الوزن")} 
+    else if  (MBI >= 18.5 && BMI < 25 ){console.log ("وزن صحي")} 
+    else   (BMI >= 25 ){console.log ("لديك زيادة في الوزن")}    
+}
+
+function calculate(weight, height) {
+    document.getElementById("caculate")  
+   
+}
+let bmiResult = BMI (60, 1.6);
+
+ document.getElementById(ID : result).innerText="calculate";
